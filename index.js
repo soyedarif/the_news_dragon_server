9ca54bb07e26121fc0b03859e4c8d51f0c0f5d35
soyedarif <soyedariff@gmail.com>
2023-04-24 18:04:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app= express()
+const cors=require('cors')
+const port= process.env.PORT || 5000;
+
+const categories=require('./data/catagories.json')
+
+app.use(cors())
+
+app.get('/', (req,res)=>{
+    res.send('Dragon is Flying')
+})
+app.get('/categories',(req,res)=>{
+    res.send(categories)
+})
+
+app.listen(port,()=>console.log(`Dragon API is running on port: ${port}`))
